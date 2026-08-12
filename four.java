ebe360ce0ddcac5534c20209ceb6b0ccb46582f2
Nawaz <nawaz@Nawazs-MacBook-Air.local>
2026-08-12 14:03:29 +0530

    282A bit++

--- four.java
+++ four.java
@@ -1,4 +1,20 @@
+import java.util.*;
 public class four {
     
-    
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int x = 0;
+        String[] arr = new String[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.next();
+        }
+        for(int i=0;i<n;i++){
+            if(arr[i].equals("X++") || arr[i].equals("++X"))x++;
+            else x--;
+        }
+        System.out.println(x);
+    }
+
 }
