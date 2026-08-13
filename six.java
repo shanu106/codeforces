4b42794a805f5007350d3a1859ada35fb2f0c670
Nawaz <nawaz@Nawazs-MacBook-Air.local>
2026-08-13 22:30:16 +0530

    better approch for domino piling 50A

--- six.java
+++ six.java
@@ -7,17 +7,19 @@ public class six {
         int n = sc.nextInt();
         int m = sc.nextInt();
 
-        if(n%2==0 && m%2==0){
-            System.out.println((n/2)*m);
-        }
-        if(n%2!=0 && m%2!=0){
-            System.out.println(n/2*m +(m-1)/2);
-        }
-        if(n%2==0 && m%2!=0){
-            System.out.println((m/2*n)+n/2);
-        }
-        if(n%2!=0 && m%2==0){
-            System.out.println((n/2*m)+m/2);
-        }
+        System.out.println((n*m)/2);
+
+        // if(n%2==0 && m%2==0){
+        //     System.out.println((n/2)*m);
+        // }
+        // if(n%2!=0 && m%2!=0){
+        //     System.out.println(n/2*m +(m-1)/2);
+        // }
+        // if(n%2==0 && m%2!=0){
+        //     System.out.println((m/2*n)+n/2);
+        // }
+        // if(n%2!=0 && m%2==0){
+        //     System.out.println((n/2*m)+m/2);
+        // }
     }
 }
